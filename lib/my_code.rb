3520b65b689621fa7c98d9d6b5cfeeba68005d04
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 09:53:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,27 +1,10 @@
 # Your Code Here
-def map_to_negativize(source_array)
-  source_array.map {|x| -x}
+def map(array)
+  new = []
+  i =0 
+  while array[i]< array[i].length do
+    push.(yield(array[i]))
+    i +=1 
+  end
+  new
 end
-
- def map_to_no_change(source_array) 
-   source_array.map {|x| x}
- end
- 
- def map_to_double(source_array)
-source_array.map {|x| x*2}
-end
-
-def map_to_square(source_array)
-  source_array.map {|x| x*x}
-end
-def reduce_to_total(source_array,starting_point=0)
-  return source_array.reduce(starting_point){|total, element| total+element}
-end
-def reduce_to_all_true(source_array)
-  return source_array.reduce{ |x, point| !!x && !!point }
-end
-
-def reduce_to_any_true(source_array)
-   return source_array.reduce{ |x, point| !!x || !!point } 
-end
-
